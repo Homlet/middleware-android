ff7e4262e6de635cc76d78734ac9af86dc41ca0d
Homlet <samlhub@gmail.com>
2018-03-12 18:30:35 +0000

    Move binding from api constructor to separate method to improve lifecycle handling.

--- api/src/main/java/uk/ac/cam/seh208/middleware/api/Middleware.java
+++ api/src/main/java/uk/ac/cam/seh208/middleware/api/Middleware.java
@@ -13,8 +13,6 @@ import uk.ac.cam.seh208.middleware.common.IntentData;
 import uk.ac.cam.seh208.middleware.common.MiddlewareCommand;
 import uk.ac.cam.seh208.middleware.common.Polarity;
 import uk.ac.cam.seh208.middleware.common.exception.BadHostException;
-import uk.ac.cam.seh208.middleware.common.exception.BadSchemaException;
-import uk.ac.cam.seh208.middleware.common.exception.EndpointCollisionException;
 
 import static uk.ac.cam.seh208.middleware.api.RemoteUtils.callSafe;
 
@@ -25,28 +23,48 @@ import static uk.ac.cam.seh208.middleware.api.RemoteUtils.callSafe;
 public class Middleware {
 
     /**
+     * Reference to the owning context, on which binding takes place.
+     */
+    private Context context;
+
+    /**
      * Connection to the remote middleware service.
      */
     private MiddlewareServiceConnection connection;
 
 
     /**
-     * Bind the given context to the middleware service, internally tracking the
-     * service connection within this object.
+     * Store a reference to the owning context, on which binding is to take place.
      *
      * @param context Any context within the client application, on which the binding takes place.
      */
-    public Middleware(Context context) throws MiddlewareDisconnectedException {
+    public Middleware(Context context) {
+        this.context = context;
+        connection = new MiddlewareServiceConnection();
+    }
+
+    /**
+     * Bind the owning context to the service, allowing communication to take
+     * place. This method must be called before any API calls can take effect.
+     */
+    public void bind() {
         // Create a new intent for starting the middleware service (if not already started).
         Intent intent = new Intent();
         intent.setClassName(IntentData.SERVICE_PACKAGE, IntentData.SERVICE_NAME);
         context.startService(intent);
 
-        // Create a binding intent for the service and bind the given context to it.
-        connection = new MiddlewareServiceConnection();
+        // Reuse the intent for binding the owning context to the service.
         context.bindService(intent, connection, Context.BIND_IMPORTANT);
     }
 
+    /**
+     * Unbind the owning context from the service to prevent leakage of
+     * ServiceConnection resources. It is essential that this method is
+     * called before the owning context is destroyed.
+     */
+    public void unbind() {
+        context.unbindService(connection);
+    }
 
     public Endpoint getEndpoint(@NonNull String name) {
         return new Endpoint(connection, name);
